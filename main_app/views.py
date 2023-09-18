62f8b7ef782f1f87ff7b43c42a7f8bbff0a9f002
kxiside <swendside@gmail.com>
2023-09-18 12:49:44 -0700

    views and urls update

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, redirect
 
 # Import CBV's
-from django.views.generic.edit import HomeView, CreateView, UpdateView, DeleteView
-from django.views.generic.detail import DetailView
-from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Import Models
 from .models import Workout, Exercise, Set
 
 # Create your views here.
-class Home(HomeView):
-    template_name = 'home.html'
+def home(request):
+    return render(request, 'home.html')
 
-class WorkoutList(ListView):
-    model = Workout
-    template_name = 'workouts/index.html'
+def about(request):
+    return render(request, 'about.html')
 
-class WorkoutDetail(DetailView):
-    model = Workout
-    template_name = 'workouts/detail.html'
+def workouts_index(request):
+    workouts = Workout.objects.all()
+    return render(request, 'workouts/index.html', { 'workouts': workouts})
+
+def workouts_detail(request, workout_id):
+    workout = Workout.objects.get(id=workout_id)
+    # add form here
+    return render(request, 'workouts/detail.html', {'workout': workout})
 
 class WorkoutCreate(CreateView):
     model = Workout
