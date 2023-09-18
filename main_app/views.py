588acf72eed8dd50537cee2a0d816e4ed164c912
kxiside <swendside@gmail.com>
2023-09-18 07:14:08 -0700

    index and detail view functions as well as urls. Created the workouts template with index and detail html files

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+# Import Models
+from.models import Workout, Exercise, Set
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def workouts_index(request):
+    workouts = Workout.objects.all()
+    return render(request, 'workouts/index.html', { 'workouts': workouts})
+
+def workouts_detail(request, workout_id):
+    workout = Workout.objects.get(id=workout_id)
+    # add form here
+    return render(request, 'workouts/detail.html', {'workout': workout})
