597173b67ae6436224120362682a783c99635208
kxiside <swendside@gmail.com>
2023-09-18 11:09:47 -0700

    class bases view update for home index and detail

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, redirect
 
 # Import CBV's
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import HomeView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 
 # Import Models
 from .models import Workout, Exercise, Set
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+class Home(HomeView):
+    template_name = 'home.html'
 
-def workouts_index(request):
-    workouts = Workout.objects.all()
-    return render(request, 'workouts/index.html', { 'workouts': workouts})
+class WorkoutList(ListView):
+    model = Workout
+    template_name = 'workouts/index.html'
 
-def workouts_detail(request, workout_id):
-    workout = Workout.objects.get(id=workout_id)
-    # add form here
-    return render(request, 'workouts/detail.html', {'workout': workout})
+class WorkoutDetail(DetailView):
+    model = Workout
+    template_name = 'workouts/detail.html'
 
 class WorkoutCreate(CreateView):
     model = Workout
